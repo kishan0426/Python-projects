1d6adc3778d00def488ef7c4f9a876f455b73708
kisha <kishan0426@gmail.com>
2023-01-10 17:52:39 +0530

    What is this project?
    Initial commit
    
    This is an app that creates a task list to be added for today based on user specification with the following options for the user to choose.
    1) Add a task for today
    2) View the added tasks
    3) Exit from the program

--- venv/Task list for today.py
+++ venv/Task list for today.py
@@ -7,7 +7,7 @@ tasks = []
 flag = True
 while flag == True:
     """Add a selection and perform add, view and exit"""
-    selection = input("Type 1 for add ,2 for view, or 3 for exit: ")
+    selection = input("Type 1 for add ,2 for view, 3 for edit and 4 for exit: ")
 
     if selection == '1':
         task = input("Enter a todo: ")
@@ -16,6 +16,13 @@ while flag == True:
         for x in tasks:
             print(x.title())
     elif selection == '3':
+        new_idx = int(input("Enter the item to be edited and updated on the list, there are {0} items in list : ".format(len(tasks))))
+        new_idx -= 1
+        tasks[new_idx] = input("Enter the task to be updated: ")
+        print("Item is successfully updated")
+        for x in tasks:
+            print(x)
+    elif selection == '4':
         flag = 'False'
     else:
         print("Enter the correct option number given above")
